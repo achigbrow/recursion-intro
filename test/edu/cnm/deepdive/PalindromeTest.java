f695335a85059ea4224eea0f9671890f5536292f
Alana Chigbrow <alana.chigbrow@gmail.com>
2019-06-03 14:09:34 -0600

    Added Fibonacci class

--- test/edu/cnm/deepdive/PalindromeTest.java
+++ test/edu/cnm/deepdive/PalindromeTest.java
@@ -9,13 +9,14 @@ public class PalindromeTest {
 
   @Test
   public void isPalindrome() {
-    String [] testArray = {"radar", "race car", "A man, a plan, a canal - Panama!"};
+    String [] testArray = {"radar", "race car", "A man, a plan, a canal - Panama!", "eeee",
+    "a", "eve", "", "deified"};
 
     for (int i = 0; i < testArray.length; i++) {
       String testValue = testArray[i];
       assertTrue(Palindrome.isPalindrome(testValue));
     }
-    String [] testFalseArray = {"test", "Bob's Burgers", "Palindrome"};
+    String [] testFalseArray = {"test", "Bob's Burgers", "Palindrome", "hi", "1234", "eve is eve"};
 
     for (int i = 0; i < testFalseArray.length; i++) {
       String testFalseValue = testFalseArray[i];
