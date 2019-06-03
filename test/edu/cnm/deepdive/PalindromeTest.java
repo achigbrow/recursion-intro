4e9f5c95fcf4203afc256b771aba7170df975a34
Alana Chigbrow <alana.chigbrow@gmail.com>
2019-06-03 10:59:10 -0600

    Added test

---
+++ test/edu/cnm/deepdive/PalindromeTest.java
@@ -0,0 +1,22 @@
+package edu.cnm.deepdive;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PalindromeTest {
+
+  @Test
+  public void isPalindrome() {
+    String [] testArray = {"radar", "AWAY", "race car", "A man, a plan, a canal - Panama!"};
+
+    for (int i = 0; i <= testArray.length; i++) {
+      String test = testArray.toString();
+      isPalindrome();
+      return;
+    }
+
+
+
+  }
+}
