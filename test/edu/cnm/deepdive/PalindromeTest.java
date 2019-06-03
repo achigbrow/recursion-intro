c15d93a5379ab112ab45d09b8a7668629769d981
Alana Chigbrow <alana.chigbrow@gmail.com>
2019-06-03 11:59:24 -0600

    Corrected test errors. Now they work

--- test/edu/cnm/deepdive/PalindromeTest.java
+++ test/edu/cnm/deepdive/PalindromeTest.java
@@ -2,20 +2,25 @@ package edu.cnm.deepdive;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Array;
 import org.junit.Test;
 
 public class PalindromeTest {
 
   @Test
   public void isPalindrome() {
-    String [] testArray = {"radar", "AWAY", "race car", "A man, a plan, a canal - Panama!"};
+    String [] testArray = {"radar", "race car", "A man, a plan, a canal - Panama!"};
 
-    for (int i = 0; i <= testArray.length; i++) {
-      String test = testArray.toString();
-      isPalindrome();
-      return;
+    for (int i = 0; i < testArray.length; i++) {
+      String testValue = testArray[i];
+      assertTrue(Palindrome.isPalindrome(testValue));
     }
+    String [] testFalseArray = {"test", "Bob's Burgers", "Palindrome"};
 
+    for (int i = 0; i < testFalseArray.length; i++) {
+      String testFalseValue = testFalseArray[i];
+      assertFalse(Palindrome.isPalindrome(testFalseValue));
+    }
 
 
   }
